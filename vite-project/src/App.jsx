cb2cb059570b5c72ba1e22191dcddaea4bdcf5d7
Kimberley 16 <kimberleypaladini@gmail.com>
2023-01-25 11:51:50 +0100

    farmer motion

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react";
+import { motion } from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <motion.div
+      style={{
+        color: "green",
+        fontSize: 20,
+        width: "300px",
+        height: "30px",
+        textAlign: "center",
+        border: "2px solid green",
+        margin: "40px",
+      }}
+      whileHover={{ scale: 0.5 }}
+    >
+      GeeksforGeeks
+    </motion.div>
   );
 }
 
