4eeddcaad77fd0c7feb58051e2d96ef8da20ed51
Kimberley 16 <kimberleypaladini@gmail.com>
2023-01-25 16:38:15 +0100

    changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
+import "./App.css";
 
 function App() {
   return (
-    <motion.div
-      style={{
-        color: "green",
-        fontSize: 20,
-        width: "300px",
-        height: "30px",
-        textAlign: "center",
-        border: "2px solid green",
-        margin: "40px",
-      }}
-      whileHover={{ scale: 0.5 }}
-    >
-      Porfolio
-    </motion.div>
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <motion.div
+        style={{
+          color: "green",
+          fontSize: 20,
+          width: "300px",
+          height: "30px",
+          textAlign: "center",
+          border: "2px solid green",
+          margin: "40px",
+        }}
+        whileHover={{ scale: 0.5 }}
+      >
+        Porfolio
+      </motion.div>
+    </>
   );
 }
 
