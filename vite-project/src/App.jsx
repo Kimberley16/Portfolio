3021564ec7a62f105ca5409a6b528b1ce9f140d1
Kimberley 16 <kimberleypaladini@gmail.com>
2023-01-26 15:42:56 +0100

    Create Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./App.css";
-
-function App() {
-  return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <motion.div
-        style={{
-          color: "green",
-          fontSize: 20,
-          width: "300px",
-          height: "30px",
-          textAlign: "center",
-          border: "2px solid green",
-          margin: "40px",
-        }}
-        whileHover={{ scale: 0.5 }}
-      >
-        Porfolio
-      </motion.div>
-    </>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
+
+// function App() {
+//   return (
+//     <>
+//       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+//       <motion.div
+//         style={{
+//           color: "green",
+//           fontSize: 20,
+//           width: "300px",
+//           height: "30px",
+//           textAlign: "center",
+//           border: "2px solid green",
+//           margin: "40px",
+//         }}
+//         whileHover={{ scale: 0.5 }}
+//       >
+//         Porfolio
+//       </motion.div>
+//     </>
+//   );
+// }
+
+// export default App;
