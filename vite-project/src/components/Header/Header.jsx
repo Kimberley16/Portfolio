0bf1d5eb42f474be8f1e8e38c46ac6c9ed99770a
Kimberley 16 <kimberleypaladini@gmail.com>
2023-02-01 10:34:43 +0100

    header in progress

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Header.css";
+import CTA from "./CTA";
+import ME from "../../assets/pourCV.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div classname="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Kimberley Paladini</h1>
+        <h5 className="text-light">Front end/Salesforce trainee</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" classname="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
