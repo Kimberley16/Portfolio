670a9be62c4c398ae17a1ab999e980215c9b1a6e
Kimberley 16 <kimberleypaladini@gmail.com>
2023-02-02 09:41:18 +0100

    nav ba in progressr

--- vite-project/src/components/Nav/Nav.jsx
+++ vite-project/src/components/Nav/Nav.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Nav.css";
 import { AiTwotoneHome } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { RiServiceFill } from "react-icons/ri";
+import { AiFillMessage } from "react-icons/ai";
+
 const Nav = () => {
   return (
     <nav>
-      <a href="#">
+      <a href="#" className="active">
         <AiTwotoneHome />
+      </a>
+      <a href="#about">
+        <FaUserCircle />
+      </a>
+      <a href="#experience">
+        <FaBook />
+      </a>
+      <a href="#services">
+        <RiServiceFill />
+      </a>
+      <a href="#contact">
+        <AiFillMessage />
       </a>
     </nav>
   );
