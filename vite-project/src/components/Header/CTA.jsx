d05d86a4dbd0655088e4c5638a75680049b7629a
Kimberley 16 <kimberleypaladini@gmail.com>
2023-02-03 09:25:09 +0100

    css corrections

--- vite-project/src/components/Header/CTA.jsx
+++ vite-project/src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/Paladini.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={CV} download className="btn btn-primary">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
