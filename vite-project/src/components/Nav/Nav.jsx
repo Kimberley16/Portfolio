aeea266e114945fd9d54f90115c11a08d35eea77
Kimberley 16 <kimberleypaladini@gmail.com>
2023-02-08 09:18:25 +0100

    nav bar finish

--- vite-project/src/components/Nav/Nav.jsx
+++ vite-project/src/components/Nav/Nav.jsx
@@ -5,23 +5,45 @@ import { FaUserCircle } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { RiServiceFill } from "react-icons/ri";
 import { AiFillMessage } from "react-icons/ai";
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiTwotoneHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <FaUserCircle />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <FaBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <RiServiceFill />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <AiFillMessage />
       </a>
     </nav>
