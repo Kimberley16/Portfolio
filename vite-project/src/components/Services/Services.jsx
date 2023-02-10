af988831ac128c2670f556149ba43bf48b1ff499
Kimberley 16 <kimberleypaladini@gmail.com>
2023-02-10 14:08:59 +0100

    services deleted

--- vite-project/src/components/Services/Services.jsx
+++ vite-project/src/components/Services/Services.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import "./Services.css";
+// import React from "react";
+// import "./Services.css";
 
-const Services = () => {
-  return <section id="services">Services</section>;
-};
+// const Services = () => {
+//   return (
+//   <section id="services">
+//     <h5>What I offer</h5>
+//     <h2>Services</h2>
 
-export default Services;
+//     <div className="container services__container">
+//       <article className="service">
+//         <div className="service__head">
+//           <h3></h3>
+
+//         </div>
+
+//       </article>
+
+//     </div>
+//   </section>;
+//   )
+// };
+
+// export default Services;
